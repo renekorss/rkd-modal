b048fdae7e823e83c24b7bb68356eafa0904bae5
Rene Korss <rene.korss@gmail.com>
2019-01-21 09:54:59 +0200

    Add missing .editorconfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,98 +11,94 @@ module.exports = function(grunt) {
 
     // Copys the files from the src folder to the dist folder.
     copy: {
-      js: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src',
-            src: '**.js',
-            dest: 'dist/'
-          }
-        ]
-      },
+        js: {
+            files: [{
+                expand: true,
+                cwd: 'src',
+                src: '**.js',
+                dest: 'dist/'
+            }]
+        },
     },
 
     // Minifies the javascript files.
     uglify: {
-      build: {
-        files: [{
-          expand: true,
-          cwd: 'dist/',
-          src: [
-            '*.js',
-            '!*.min.js'
-          ],
-          dest: 'dist/',
-          ext: '.min.js'
-        }]
-      }
+        build: {
+            files: [{
+                expand: true,
+                cwd: 'dist/',
+                src: [
+                    '*.js',
+                    '!*.min.js'
+                ],
+                dest: 'dist/',
+                ext: '.min.js'
+            }]
+        }
     },
 
     // Compiles the stylesheet files.
     sass: {
-      build: {
-        options: {
-          includePaths: [
-            'node_modules/'
-          ],
-          style: 'expanded',
-          sourcemap: 'none'
-        },
-        files: [{
-          expand: true,
-          cwd: 'src/',
-          src: 'rkd-modal.scss',
-          dest: 'dist/',
-          ext: '.css'
-        }]
-      }
+        build: {
+            options: {
+                includePaths: [
+                    'node_modules/'
+                ],
+                style: 'expanded',
+                sourcemap: 'none'
+            },
+            files: [{
+                expand: true,
+                cwd: 'src/',
+                src: 'rkd-modal.scss',
+                dest: 'dist/',
+                ext: '.css'
+            }]
+        }
     },
 
     // Minifies the stylesheet files.
     cssmin: {
-      build: {
-        expand: true,
-        cwd: 'dist/',
-        src: [
-          '*.css',
-          '!*.min.css'
-        ],
-        dest: 'dist/',
-        ext: '.min.css'
-      }
+        build: {
+            expand: true,
+            cwd: 'dist/',
+            src: [
+                '*.css',
+                '!*.min.css'
+            ],
+            dest: 'dist/',
+            ext: '.min.css'
+        }
     },
 
     // Lints JS files for errors
     jshint: {
-      build: {
-        files: {
-          src: ['src/**/*.js']
+        build: {
+            files: {
+                src: ['src/**/*.js']
+            }
         }
-      }
     },
 
     // Lints SASS files for errors
     sasslint: {
-      build: ['src/**/*.scss'],
+        build: ['src/**/*.scss'],
     },
 
     // Watches the project for changes and recompiles the output files.
     watch: {
+        javascript: {
+            files: 'src/**/*.js',
+            tasks: ['jshint:build', 'copy:js', 'uglify:build']
+        },
 
-      javascript: {
-        files: 'src/**/*.js',
-        tasks: ['jshint:build', 'copy:js', 'uglify:build']
-      },
-
-      sass: {
-        files: 'src/**/*.scss',
-        tasks: ['sasslint:build', 'sass:build', 'cssmin:build']
-      },
-
+        sass: {
+            files: 'src/**/*.scss',
+            tasks: ['sasslint:build', 'sass:build', 'cssmin:build']
+        },
     }
-  });
+    });
 
-  // Default taks
-  grunt.registerTask('default', ['clean', 'copy', 'uglify', 'sasslint', 'sass', 'cssmin']);
+    // Default taks
+    grunt.registerTask('default', ['clean', 'copy', 'uglify', 'sasslint', 'sass', 'cssmin']);
 };
